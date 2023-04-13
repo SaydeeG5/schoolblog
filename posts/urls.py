101ff11517444327414ed9cbd9135d564d6521cf
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-04-12 21:05:38 -0700

    first commit

---
+++ posts/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path 
+from .views import (
+    PostListView,
+    PostUpdateView,
+    PostDeleteView,
+    PostDetailView,
+    PostCreateView,
+    BlogSearchView,
+)
+
+urlpatterns = [
+    path("", PostListView.as_view(), name="list"),
+    path("<int:pk>/edit/", PostUpdateView.as_view(), name="edit"),
+    path("<int:pk>/delete/", PostDeleteView.as_view(), name="delete"),
+    path("<int:pk>/", PostDetailView.as_view(), name="detail"),
+    path("new/", PostCreateView.as_view(), name="new"),
+    path("search/", BlogSearchView.as_view(), name="search")
+
+]
---
+++ posts/views.py
@@ -0,0 +1,62 @@
+from django.views.generic import (
+    CreateView, 
+    DetailView,
+    UpdateView, 
+    DeleteView, 
+    ListView,
+)
+
+from .models import Post 
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import ( 
+    LoginRequiredMixin,
+    UserPassesTestMixin
+)
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post 
+
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post 
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "active"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post 
+    fields = ["title", "subtitle", "body", "active"]
+
+    def test_func(self):
+        user= self.request.user
+        post = self.get_object()
+        return user == post.author 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post 
+    success_url = reverse_lazy("list")
+
+    def test_func(self):
+        user= self.request.user
+        post = self.get_object()
+        return user == post.author 
+
+class BlogSearchView(ListView):
+    template_name = "posts/search.html"
+    model = Post 
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        return Post.objects.filter(title__icontains=query)| Post.objects.filter(body__icontains=query)
+        
+        
