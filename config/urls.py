d2be0858f817316560b4e93f08c81cc2aabd9364
Maria Saydee Guevarra <saydeeguevarra@gmail.com>
2023-04-26 19:33:26 -0700

    added pw reset and back btn on search

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("", include("pages.urls")),
     path("", include("posts.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("accounts.urls")), 
 ]
